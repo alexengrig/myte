ebeaaa62aa2bd16fe4528d4fd97224a04b3e9066
Grig Alex <alexengrigdev@gmail.com>
2020-04-07 20:18:02 +0300

    Rename TextEditor to TEFrame

--- src/main/java/dev/alexengrig/myte/ApplicationRunner.java
+++ src/main/java/dev/alexengrig/myte/ApplicationRunner.java
@@ -1,7 +1,9 @@
 package dev.alexengrig.myte;
 
+import dev.alexengrig.myte.ui.TEFrame;
+
 public class ApplicationRunner {
     public static void main(String[] args) {
-        new TextEditor();
+        new TEFrame();
     }
 }
